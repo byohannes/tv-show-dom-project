382b9bdec2911bc935dd0aa8a81157b7b3f5df29
byohannes <42332389+byohannes@users.noreply.github.com>
2020-04-21 19:50:14 +0100

    Level 100 - Minimal features completed

--- script.js
+++ script.js
@@ -1,12 +1,77 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+function setup () {
+  const allEpisodes = getAllEpisodes ();
+  makePageForEpisodes (allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
+function makePageForEpisodes (episodeList) {
+  const rootElem = document.getElementById ('root');
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  rootElem.classList.add ('container');
+  document.querySelector ('.container').setAttribute ('display', 'flex');
+  document.querySelector ('.container').setAttribute ('flexWrap', 'wrap');
+
+  for (let i = 0; i < episodeList.length; i++) {
+    // Creating a div element
+    let divElement = document.createElement ('Div');
+
+    divElement.setAttribute ('class', 'episode');
+    divElement.setAttribute ('overflow', 'hidden');
+    divElement.setAttribute ('background', 'white');
+    divElement.setAttribute ('borderRadius', '12px');
+    divElement.setAttribute ('margin', '1rem');
+    // Styling it
+    /*
+    divElement.style.overflow = 'hidden';
+    divElement.style.background = 'white';
+    divElement.style.borderRadius = '12px';
+    divElement.style.margin = '1rem';
+    */
+
+    // Adding h1 to it
+    let h1Elem = document.createElement ('h1');
+    h1Elem.innerHTML =
+      episodeList[i].name +
+      ' - S' +
+      episodeList[i]['season'].toString ().padStart (2, '0') +
+      '' +
+      episodeList[i]['number'].toString ().padStart (2, '0');
+    divElement.appendChild (h1Elem);
+    // Styling h1
+    h1Elem.style.textAlign = 'center';
+    h1Elem.style.padding = '1.5rem 2.5rem';
+    h1Elem.style.fontSize = '1.5rem';
+    h1Elem.style.border = '1px solid black';
+    h1Elem.style.borderRadius = '12px';
+    h1Elem.style.color = 'black';
+    h1Elem.style.margin = '0 0 2rem 0';
+    // Adding an image to it
+    let imageElem = document.createElement ('img');
+    imageElem.src = episodeList[i].image.medium;
+    divElement.appendChild (imageElem);
+    // Styling image
+    imageElem.style.width = '100%';
+    imageElem.style.height = 'auto';
+    imageElem.style.display = '1.inline-block';
+    imageElem.style.borderRadius = '5px';
+    // Adding a paragraph to it
+    let paragraph = document.createElement ('P');
+    paragraph.innerHTML = episodeList[i].summary;
+    divElement.appendChild (paragraph);
+    // Styling paragraph
+    paragraph.style.padding = '0 2.5rem 2.5rem';
+    paragraph.style.margin = '0';
+    // Appending the div element to body
+    rootElem.appendChild (divElement);
+  }
+  const info = document.createElement ('a');
+  const infoPar = document.createElement ('h3');
+  info.setAttribute ('href', 'https://www.tvmaze.com/api#licensing');
+  infoPar.innerHTML = 'The data has (originally) come from  ';  
+  info.innerHTML = 'tvmaze.com/api#licensing';
+  infoPar.appendChild (info);
+  document.body.appendChild (infoPar);
 }
 
 window.onload = setup;
