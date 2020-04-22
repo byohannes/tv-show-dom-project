bb941c5412c80cd0eb8276adcd933ed2ed916dff
byohannes <42332389+byohannes@users.noreply.github.com>
2020-04-22 23:59:40 +0100

    level 300 and updated level 200

--- script.js
+++ script.js
@@ -5,24 +5,23 @@ function setup () {
 }
 
 const rootElem = document.getElementById ('root');
-// Link to the source of data href', 'https://www.tvmaze.com/api#licensing'
-const info = document.createElement ('a');
-const infoPar = document.createElement ('h4');
-info.setAttribute ('href', 'https://www.tvmaze.com/api#licensing');
-infoPar.innerHTML = 'The data has (originally) come from  ';
-info.innerHTML = 'tvmaze.com/api#licensing';
-infoPar.appendChild (info);
-document.body.appendChild (infoPar);
+
 //a container for all episodes
 let divContainer = document.createElement ('div');
 divContainer.className = 'container';
+//Create and append select list
+let selectList = document.createElement ('select');
+selectList.id = 'mySelect';
+
 // search box
 let searchBox = document.createElement ('div');
+searchBox.appendChild (selectList);
 rootElem.appendChild (searchBox).className = 'search';
 let searchInput = document.createElement ('input');
+searchInput.placeholder = 'your search item...';
 searchBox.appendChild (searchInput);
 searchInput.setAttribute ('type', 'text');
-let searchLabel = document.createElement ('h2');
+let searchLabel = document.createElement ('h3');
 searchLabel.className = 'searchEpisode';
 searchBox.appendChild (searchLabel);
 rootElem.appendChild (divContainer);
@@ -51,6 +50,23 @@ function makePageForEpisodes (episodeList) {
     let paragraph = document.createElement ('P');
     paragraph.innerHTML = episodeList[i].summary.trim ();
     divElement.appendChild (paragraph);
+    //Create and append the options of select input
+    let option = document.createElement ('option');
+    option.value =
+      'S' +
+      episodeList[i]['season'].toString ().padStart (2, '0') +
+      'E' +
+      episodeList[i]['number'].toString ().padStart (2, '0') +
+      ' - ' +
+      episodeList[i].name;
+    option.text =
+      'S' +
+      episodeList[i]['season'].toString ().padStart (2, '0') +
+      'E' +
+      episodeList[i]['number'].toString ().padStart (2, '0') +
+      ' - ' +
+      episodeList[i].name;
+    selectList.appendChild (option);
   }
   searchFunc ();
 }
@@ -77,5 +93,13 @@ function searchFunc () {
   });
   searchLabel.innerHTML = `Displaying ${searchCount}/${epiListLength} episode(s)`;
 }
+// Link to the source of data href', 'https://www.tvmaze.com/api#licensing'
+const info = document.createElement ('a');
+const infoPar = document.createElement ('h4');
+info.setAttribute ('href', 'https://www.tvmaze.com/api#licensing');
+infoPar.innerHTML = 'The data has (originally) come from  ';
+info.innerHTML = 'tvmaze.com/api#licensing';
+infoPar.appendChild (info);
+document.body.appendChild (infoPar);
 
 window.onload = setup;
